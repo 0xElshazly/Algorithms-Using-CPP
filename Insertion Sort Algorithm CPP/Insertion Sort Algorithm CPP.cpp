8ec89b507a082b4edce61013ab575ceef464f1e5
Mohamed Ayman Elshazly <77213993+MohamedAymanSaid@users.noreply.github.com>
2022-01-29 14:30:55 +0200

    Update Insertion Sort Algorithm CPP.cpp

--- Insertion Sort Algorithm CPP/Insertion Sort Algorithm CPP.cpp
+++ Insertion Sort Algorithm CPP/Insertion Sort Algorithm CPP.cpp
@@ -52,5 +52,11 @@ int main(){
 	[Uses]: Insertion sort is used when number of elements is small. 
 	It can also be useful when input array is almost sorted, 
 	only few elements are misplaced in complete big array.
+	
+	Performance
+
+		- Worst case performance: O(n^2)
+		- Best case performance: O(n)
+		- Average case performance: O(n^2)
 */
 // This code is contributed by Mohamed Ayman.
